d04d8822f8108f4355d8b1ebc2c55a8839511abf
Ana Cruz <anmacruro4@gmail.com>
2023-07-29 23:03:09 -0500

    api

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const URL_ROOT = 'https://dwfs16-m3s21-ejercicio-auth.onrender.com/api/auth' //link de la api creada en render
+const URL_ROOT = 'https://comercio-be.onrender.com/api/auth'
 
 const loginService = async (form) => {
     const resp = await axios.post(`${URL_ROOT}/login`, form)
--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const URL_ROOT = `http://localhost:4000/api/productos` //link de la base de datos
+const URL_ROOT = `https://comercio-be.onrender.com/api/productos`
 
 const getProductsService = async () => {
     const response = await axios.get(`${URL_ROOT}`)
